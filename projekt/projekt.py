bd93afa37360932ef8e925f2c10c03c014931585
RosenFabian <131650165+RosenFabian@users.noreply.github.com>
2024-03-18 11:25:12 +0100

    Update projekt.py

--- projekt/projekt.py
+++ projekt/projekt.py
@@ -1,1 +1,19 @@
+from bottle import get, post, run, response, request
+from urllib.parse import unquote
+import sqlite3
 
+# Set-up
+PORT = 7007
+# sqlite3 lab3.sqlite < lab3.sql
+db = sqlite3.connect("/Users/josefinefrid/Desktop/Databasteknik/lab2/Databasteknik-lab2/lab3/lab3.sqlite")
+db.execute("PRAGMA foreign_keys = ON")
+
+#functions start here 
+
+
+
+
+
+#functions end here 
+
+run(host='localhost', port=PORT)
