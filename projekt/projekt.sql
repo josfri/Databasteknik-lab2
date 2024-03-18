660e0a60b1a07810a159a997219e5c14773df738
RosenFabian <131650165+RosenFabian@users.noreply.github.com>
2024-03-18 10:26:51 +0100

    Update projekt.sql

--- projekt/projekt.sql
+++ projekt/projekt.sql
@@ -1,1 +1,63 @@
 
+-- Frågan är om det ska vara decimaler eller inte i siffrorna, dvs om det ska vara INT eller DECIMAL
+CREATE TABLE Warehouse (
+    ingredient TEXT,
+    amount DECIMAL NOT NULL,
+    unit TEXT,
+    last_delivery_time TIME,
+    last_delivery_amount DECIMAL NOT NULL,
+    PRIMARY KEY (ingredient)
+);
+
+CREATE TABLE Recipe (
+    product_name TEXT,
+    PRIMARY KEY (product_name)
+);
+
+CREATE TABLE Recipe_quantity (
+    product_name TEXT,
+    ingredient TEXT,
+    amount DECIMAL NOT NULL,
+    FOREIGN KEY (product_name) REFERENCES Recipe(product_name),
+    FOREIGN KEY (ingredient) REFERENCES Warehouse(ingredient)
+);
+
+CREATE TABLE Customer (
+    customer_name TEXT,
+    customer_address TEXT,
+    PRIMARY KEY (customer_name)
+);
+-- Ändrade address till customer_address då address highlightades i blått i vs code
+
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT,
+    delivery_day DATE,
+    delivery_time TIME,
+    successfully_delivered BOOLEAN NOT NULL,
+    customer_name TEXT,
+    product_name TEXT,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_name) REFERENCES Customer(customer_name),
+    FOREIGN KEY (product_name) REFERENCES Recipe(product_name)
+);
+
+CREATE TABLE Pallet (
+    pallet_nbr INT AUTO_INCREMENT,
+    production_date DATE NOT NULL,
+    production_time TIME NOT NULL,
+    blocked BOOLEAN NOT NULL,
+    product_name TEXT,
+    order_id INT,
+    PRIMARY KEY (pallet_nbr),
+    FOREIGN KEY (product_name) REFERENCES Recipe(product_name),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
+CREATE TABLE Order_item (
+    order_id INT,
+    pallet_nbr INT,
+    nbr_pallets INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (pallet_nbr) REFERENCES Pallet(pallet_nbr)
+);
+
