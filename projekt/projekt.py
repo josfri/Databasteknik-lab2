656f378a7f7ba589ddfa3da9ba2c5bdb231cff98
frekre <131352568+frekre@users.noreply.github.com>
2024-03-19 13:40:35 +0100

    fremjasfunktioner

--- projekt/projekt.py
+++ projekt/projekt.py
@@ -16,9 +16,72 @@ db.execute("PRAGMA foreign_keys = ON")
 
 #Add and check customers
 
-#Add and check ingredients
+#----------Add and check ingredients- Fremjas kod----------------
 
-#Add and check recipes/ookies
+@post('/ingredients')
+def add_ingredient(): 
+    new_ingredient = request.json #hämta json objektet 
+    c = db.cursor()
+    c.execute( 
+        """
+            INSERT 
+            INTO Warehouse(ingredient, unit)
+            VALUES (?, ?)
+        """, 
+        [ new_ingredient('ingredient'), new_ingredient('unit'),]
+    )
+  
+    url_encoded_ingredient = quote(new_ingredient('ingredient'))
+    response.status = 201
+    return {
+        'location' : f'/ingredients/' + url_encoded_ingredient 
+    }
+
+@post('/ingredients/<ingredient>/deliveries')
+def update_ingredient(ingredient): 
+    new_delivery = request.json #hämta json objektet 
+    c = db.cursor()
+    c.execute( 
+        """
+            UPDATE Warehouse 
+            SET last_delivery_time = ?, 
+                last_delivery_amount = ?,
+                amount = amount + ?, 
+            WHERE ingredient = ?
+            RETURNING ingredient, amount, unit
+        """, [new_delivery('deliveryTime'), new_delivery('quantity'), new_delivery('quantity'), ingredient]
+    ) # här kan man göra en trigger istället i databsen som ändrar amount i warehpuse när vi lägger till en delivery. OBS vet ej om det funkar som det är nu!! 
+  
+    found = [{ 
+        "ingredient": ingredient, 
+        "quantity": amount, 
+        "unit": unit} for ingredient, amount, unit in c
+    ]
+    response.status = 201
+    return {
+        "data": found
+    }
+
+@get('/ingredients')
+def get_ingredients(): 
+    c = db.cursor()
+    c.execute(
+        """
+        SELECT ingredient, amount, unit
+        FROM Warehouse 
+        """
+    )
+    found = [{ 
+        "ingredient": ingredient, 
+        "quantity": amount, 
+        "unit": unit} for ingredient, amount, unit in c
+    ]
+    response.status = 200
+    return {
+        "data": found
+    }
+
+#--------Add and check recipes/ookies-----------
 
 #Add and check pallets
 
